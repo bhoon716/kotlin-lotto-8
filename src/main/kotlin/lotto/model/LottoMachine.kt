4ea99c7477f7af0746c8e8d2767cac8c85bbd566
박병훈 <leafloveu@naver.com>
2025-11-17 22:31:46 +0900

    feat(lottoMachine): 로또 구입 기능 추가
    - 잘못된 구입 금액 예외 처리 추가

--- src/main/kotlin/lotto/model/LottoMachine.kt
+++ src/main/kotlin/lotto/model/LottoMachine.kt
@@ -1,16 +1,27 @@
 package lotto.model
 
+import lotto.error.ErrorCode
 import lotto.model.strategy.LottoIssueStrategy
 import java.util.stream.IntStream
 
 class LottoMachine(private val issueStrategy: LottoIssueStrategy) {
 
-    fun purchase(money: Int): PurchaseLottos {
-        val amount = money / 1000
-        val lottos = IntStream.range(0, amount)
+    fun purchase(amount: Int): PurchaseLottos {
+        validate(amount)
+        val count = amount / 1_000
+        val lottos = IntStream.range(0, count)
             .mapToObj { i -> issueStrategy.issue() }
             .map { numbers -> Lotto(numbers) }
             .toList()
         return PurchaseLottos(lottos)
+    }
+
+    private fun validate(amount: Int) {
+        if(amount < 1_000 || amount > 100_000) {
+            throw IllegalArgumentException(ErrorCode.INVALID_PURCHASE_AMOUNT.message())
+        }
+        if(amount % 1_000 != 0) {
+            throw IllegalArgumentException(ErrorCode.INVALID_PURCHASE_AMOUNT.message())
+        }
     }
 }
