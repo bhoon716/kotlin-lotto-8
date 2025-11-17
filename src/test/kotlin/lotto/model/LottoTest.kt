acfb96ebf01d86d0f412931d59b4f906c7ed2fcc
박병훈 <leafloveu@naver.com>
2025-11-17 21:56:52 +0900

    test(lotto): 두 로또 비교 테스트 추가 (RED)

--- src/test/kotlin/lotto/model/LottoTest.kt
+++ src/test/kotlin/lotto/model/LottoTest.kt
@@ -1,9 +1,13 @@
 package lotto.model
 
+import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import java.util.*
 
 class LottoTest {
     @Test
@@ -35,5 +39,33 @@ class LottoTest {
         // then
         assertThat(result1).isTrue
         assertThat(result2).isFalse
+    }
+
+    @DisplayName("로또끼리 비교해서 같은 번호가 몇 개인지 반환해주는 메서드 테스트")
+    @ParameterizedTest
+    @CsvSource(
+        value = [
+            "10,11,12,13,14,15:6",      // 6개 일치
+            "11,12,13,14,15,16:5",      // 5개 일치
+            "12,13,14,15,16,17:4",      // 4개 일치
+            "13,14,15,16,17,18:3",      // 3개 일치
+            "14,15,16,17,18,19:2",      // 2개 일치
+            "15,16,17,18,19,20:1",      // 1개 일치
+            "16,17,18,19,20,21:0"       // 0개 일치
+        ], delimiter = ':'
+    )
+    fun countMatchedNumbersTest(numbers: String, expected: Int) {
+        // given
+        val lottoNumbers = numbers.split(",")
+            .map(String::toInt)
+            .toList()
+        val lotto1 = Lotto(lottoNumbers)
+        val lotto2 = Lotto(listOf(10, 11, 12, 13, 14, 15))
+
+        // when
+        val equalNumberCount: Int = lotto1.countMatchedNumbers(lotto2)
+
+        // then
+        Assertions.assertThat(equalNumberCount).isEqualTo(expected)
     }
 }
