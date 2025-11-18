a9a153e306577930c0e8e7946a2b053455a5b67b
박병훈 <leafloveu@naver.com>
2025-11-18 18:41:01 +0900

    feat(purchaseLottos):구매한 로또 전부 당첨 확인 기능 추가

--- src/main/kotlin/lotto/model/PurchaseLottos.kt
+++ src/main/kotlin/lotto/model/PurchaseLottos.kt
@@ -1,4 +1,16 @@
 package lotto.model
 
+import lotto.dto.TotalLottoResult
+
 class PurchaseLottos(private val lottos: List<Lotto>) {
+
+    fun judgeAll(winningLotto: WinningLotto): TotalLottoResult {
+        return TotalLottoResult(
+            lottos
+                .map { winningLotto.judge(it) }
+                .map { LottoPrize.determine(it) }
+                .groupingBy { it }
+                .eachCount()
+        )
+    }
 }
