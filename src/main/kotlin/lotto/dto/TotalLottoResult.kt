6d51287e48577e931868f8830540a93d9d978c51
박병훈 <leafloveu@naver.com>
2025-11-18 19:14:09 +0900

    feat(totalResult): 당첨 수익률 계산 기능 추가

--- src/main/kotlin/lotto/dto/TotalLottoResult.kt
+++ src/main/kotlin/lotto/dto/TotalLottoResult.kt
@@ -1,5 +1,24 @@
 package lotto.dto
 
 import lotto.model.LottoPrize
+import java.math.BigDecimal
+import java.math.RoundingMode
 
-data class TotalLottoResult(val lottoResultsByPrize: Map<LottoPrize, Int>)
+data class TotalLottoResult(val lottoResultsByPrize: Map<LottoPrize, Int>) {
+
+    fun returnRate(): Double {
+        val totalReward = lottoResultsByPrize.entries
+            .sumOf { (prize, count) -> prize.reward * count }
+
+        val totalAmount = lottoResultsByPrize.values
+            .sumOf { it * 1_000.0 }
+
+        if (totalAmount == 0.0) {
+            return 0.0
+        }
+
+        val returnRatePercent = totalReward / totalAmount * 100
+
+        return BigDecimal(returnRatePercent).setScale(1, RoundingMode.HALF_UP).toDouble()
+    }
+}
