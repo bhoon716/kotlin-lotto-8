8c44e0ccbe4cc27c955a8175e17b0e2fa856a700
박병훈 <leafloveu@naver.com>
2025-11-18 19:24:00 +0900

    feat(output): n개 구매 문구 출력 기능 추가

--- src/main/kotlin/lotto/view/OutputView.kt
+++ src/main/kotlin/lotto/view/OutputView.kt
@@ -5,4 +5,8 @@ class OutputView {
     fun printEnterPurchaseAmount() {
         println("구입금액을 입력해주세요.")
     }
+
+    fun printPurchaseAmount(count: Int) {
+        println("${count}개를 구매했습니다.")
+    }
 }
